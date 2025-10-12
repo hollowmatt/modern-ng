c8b38dcf685c14e28e657edbc8d3cffc2531f4fe
Matt Holloway <hollowmatt@gmail.com>
2025-10-12 19:25:30 +0100

    more fun with modern ng

---
+++ ch1/hrms/src/app/pages/login.ts
@@ -0,0 +1,22 @@
+import { FormsModule } from "@angular/forms";
+import { Component } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.html',
+  standalone: true,
+  imports: [FormsModule],
+})
+
+export class LoginComponent {
+    credentials = { email: '', password: '' };
+
+    constructor(private authService: AuthService) {}
+
+    login() {
+        if (this.credentials.email && this.credentials.password) {
+            this.authService.login(this.credentials).subscribe();
+        }
+    }
+}
