903f10d9c18c8ef68aa5f09968cd4ae0bd014ae5
Matt Holloway <hollowmatt@gmail.com>
2025-10-18 15:46:02 +0100

    wire in employee service and list component

--- ch1/hrms/src/app/pages/employees/employee-list-component.ts
+++ ch1/hrms/src/app/pages/employees/employee-list-component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-employee-list-component',
-  imports: [],
-  templateUrl: './employee-list-component.html'
+  imports: [AsyncPipe],
+  templateUrl: './employee-list-component.html',
+  standalone: true,
 })
 export class EmployeeListComponent {
+  employeeService =  inject(EmployeeService);
+  employees$ = this.employeeService.getEmployees();
+  isConfirmationOpen = false;
 
+  showConfirmationDialog(){
+    this.isConfirmationOpen = true;
+  }
 }
--- ch1/hrms/src/app/services/employee.service.ts
+++ ch1/hrms/src/app/services/employee.service.ts
@@ -3,4 +3,11 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class EmployeeService {
   // Employee service logic here
+  getEmployees() {
+    // Return an observable or promise with employee data
+    return[
+      { id: 1, firstName: 'John', lastName: 'Doe', position: 'Farmer' }, 
+      { id: 2, firstName: 'Jane', lastName: 'Smith', position: 'Engineer' }
+    ];
+  }
 }
