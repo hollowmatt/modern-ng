9acc7e11be2ebd7fcd274c9d41f283cc20261baa
Matt Holloway <hollowmatt@gmail.com>
2025-10-19 20:17:24 +0100

    wire detail to component from service and router

--- ch1/hrms/src/app/pages/employees/employee-detail-component.ts
+++ ch1/hrms/src/app/pages/employees/employee-detail-component.ts
@@ -1,14 +1,27 @@
 import { AsyncPipe, NgComponentOutlet } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-employee-detail-component',
-  imports: [AsyncPipe, NgComponentOutlet],
+  imports: [RouterModule],
   templateUrl: './employee-detail-component.html',
   standalone: true,
 })
 export class EmployeeDetailComponent {
-  //coming soon 
+  employee = {
+    id:0,
+    firstName:'',
+    lastName:'',
+    position:'',
+    age:0
+  }
+  constructor(private employeeService: EmployeeService, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.employee.id = params['id'];
+      this.employeeService.getEmployeeById(+this.employee.id);
+    })
+  }
 }
